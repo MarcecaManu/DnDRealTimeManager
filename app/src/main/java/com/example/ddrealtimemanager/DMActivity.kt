75486d820936eb2bae16c27dc42c384a5b24b23a
MarcecaManu <marcecamanu@gmail.com>
2022-05-18 18:59:46 +0200

    Section selection added

---
+++ app/src/main/java/com/example/ddrealtimemanager/DMActivity.kt
@@ -0,0 +1,12 @@
+package com.example.ddrealtimemanager
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.Window
+
+class DMActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_dmactivity)
+    }
+}
--- app/src/main/java/com/example/ddrealtimemanager/MainActivity.kt
+++ app/src/main/java/com/example/ddrealtimemanager/MainActivity.kt
@@ -1,12 +1,22 @@
 package com.example.ddrealtimemanager
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        btnDM.setOnClickListener {
+            startActivity(Intent(this, DMActivity::class.java))
+        }
+
+        btnPlayer.setOnClickListener {
+            startActivity(Intent(this, PlayerActivity::class.java))
+        }
 
 
     }
